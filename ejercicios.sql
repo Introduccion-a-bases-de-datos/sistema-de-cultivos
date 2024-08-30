498af0f46e6f7c4b05d8f0def3074ee46bb49f5f
Daniel Saldarriaga López <dsaldarriaga@prevalentware.com>
2024-08-30 18:07:07 -0500

    Create ejercicios.sql

---
+++ ejercicios.sql
@@ -0,0 +1,29 @@
+-- 0. Realizar un analisis exploratorio de datos
+
+
+-- Queries simples
+
+-- 1.  Obtener el total recolectado por cada cultivo en el 2023
+
+-- 2. Listar clientes y el número de despachos que han tenido durante el 2023, ordenados por la cantidad de despachos
+
+-- 3. Calcular el precio promedio de cada cultivo a lo largo del tiempo:
+
+-- 4. Identificar los lotes con menor rendimiento en el 2023 y ordenar por cantidad recolectada:
+
+
+-- Queries con Having:
+
+-- 5. Encontrar cultivos cuyo promedio de precios haya sido superior a un valor específico en el último año:
+
+-- 6. Listar las fincas que han tenido más de 10 lotes cultivados:
+
+-- 7. Identificar los usuarios que han recogido más de 1000 unidades en total:
+
+-- 8. Determinar los meses en los cuales el valor total de despachos superó 1500 unidades:
+
+
+-- SubQueries:
+
+-- 9. Listar los clientes que han hecho pedidos en cada uno de los últimos tres meses:
+
