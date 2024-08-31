7204cf7e9efb93d65b50451c13b9bddf60546de5
Daniel Saldarriaga López <dsaldarriaga@prevalentware.com>
2024-08-30 21:05:47 -0500

    Update ejercicios.sql

--- ejercicios.sql
+++ ejercicios.sql
@@ -98,6 +98,7 @@ having sum(r.cantidad)>1000
 
 -- 8. Determinar los meses en los cuales el valor total de despachos superó 1500 unidades:
 
+-- 10. Calcular los lotes que tuvieron un ingreso proyectado mayor a 100 millones en el 2023. El ingreso proyectado se calcula como el precio multiplicado por la cantidad de producto recogido.
 
 -- SubQueries:
 
